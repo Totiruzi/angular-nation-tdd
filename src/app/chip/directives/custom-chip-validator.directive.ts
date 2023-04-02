9a71c802c5bf845f891121c2b061020a5efcb524
Agent RR <rconr007@gmail.com>
2023-04-02 14:53:45 -0400

    RT-0402-253 - Created new folder for directives

---
+++ src/app/chip/directives/custom-chip-validator.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input } from '@angular/core';
+import { ChipComponent } from '../chip.component';
+
+
+@Directive({
+  selector: '[appCustomChipValidator]'
+})
+export class CustomChipValidatorDirective {
+  @Input('customChipValidator') maxChips: number;
+  
+  constructor(private chipComponent: ChipComponent) { }
+
+  ngOnChanges() {
+    if (this.maxChips) {
+      this.chipComponent.validateChipsCount(this.maxChips);
+    }
+  }
+
+}
